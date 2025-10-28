8229c79720e93e9f5b050a7ddde5b5076ae3bb13
ivakavcic <enquiries@metoffice.gov.uk>
2025-10-28 08:23:05 +0000

    ticket:750 : thomasbendall : Change blending weights
    git-svn-id: https://metomi/svn/lfric_apps.xm/main/trunk@15020 3896877f-8c05-4358-bc88-7947bb4308c7

--- science/gungho/source/kernel/limited_area/set_blending_weights_w2_kernel_mod.F90
+++ science/gungho/source/kernel/limited_area/set_blending_weights_w2_kernel_mod.F90
@@ -17,7 +17,7 @@ module set_blending_weights_w2_kernel_mod
                                         GH_INC,               &
                                         GH_INTEGER, GH_BASIS, &
                                         CELL_COLUMN,          &
-                                        STENCIL, CROSS
+                                        STENCIL, CROSS2D
   use fs_continuity_mod,         only : W3, W2
   use constants_mod,             only : r_def, i_def
   use kernel_mod,                only : kernel_type
@@ -33,10 +33,10 @@ module set_blending_weights_w2_kernel_mod
 
   type, public, extends(kernel_type) :: set_blending_weights_w2_kernel_type
     private
-    type(arg_type) :: meta_args(3) = (/                              &
-         arg_type(GH_FIELD,   GH_REAL, GH_INC, W2),                  & ! weights_field
-         arg_type(GH_FIELD,   GH_REAL, GH_READ, W3, STENCIL(CROSS)), & ! onion_layers
-         arg_type(GH_SCALAR,  GH_INTEGER, GH_READ)                   & ! depth
+    type(arg_type) :: meta_args(3) = (/                                &
+         arg_type(GH_FIELD,   GH_REAL, GH_INC, W2),                    & ! weights_field
+         arg_type(GH_FIELD,   GH_REAL, GH_READ, W3, STENCIL(CROSS2D)), & ! onion_layers
+         arg_type(GH_SCALAR,  GH_INTEGER, GH_READ)                     & ! depth
          /)
     integer :: operates_on = CELL_COLUMN
   contains
@@ -51,26 +51,30 @@ module set_blending_weights_w2_kernel_mod
 contains
 
 !> @param[in] nlayers      Number of layers
-!> @param[in,out] weights_field The output field
-!> @param[in] onion_layers The input field
-!> @param[in] depth     Depth of the weight array
-!> @param[in] ndf_out   Number of degrees of freedom for weights_field
-!> @param[in] undf_out  Total number of degrees of freedom for weights_field
-!> @param[in] map_out   Dofmap for the cell at the base of the column for weights_field
-!> @param[in] ndf_in    Number of degrees of freedom for onion_layers
-!> @param[in] undf_in   Total number of degrees of freedom for onion_layers
-!> @param[in] map_in    Dofmap for the cell at the base of the column for onion_layers
-subroutine set_blending_weights_w2_code( nlayers,       &
-                                         weights_field, &
-                                         onion_layers,  &
-                                         stencil_size,  &
-                                         stencil_map,   &
-                                         depth,     &
-                                         ndf_out,   &
-                                         undf_out,  &
-                                         map_out,   &
-                                         ndf_in,    &
-                                         undf_in,   &
+!> @param[in,out] weights_field The output weights field
+!> @param[in] onion_layers   The onion layer field
+!> @param[in] stencil_sizes  Sizes of the branches of the cross stencil
+!> @param[in] stencil_max    Maximum branch size of the cross stencil
+!> @param[in] stencil_map    The stencil map
+!> @param[in] depth          Depth of the weight array
+!> @param[in] ndf_out        Number of degrees of freedom for weights_field
+!> @param[in] undf_out       Total number of degrees of freedom for weights_field
+!> @param[in] map_out        Dofmap for the cell at the base of the column for weights_field
+!> @param[in] ndf_in         Number of degrees of freedom for onion_layers
+!> @param[in] undf_in        Total number of degrees of freedom for onion_layers
+!> @param[in] map_in         Dofmap for the cell at the base of the column for onion_layers
+subroutine set_blending_weights_w2_code( nlayers,        &
+                                         weights_field,  &
+                                         onion_layers,   &
+                                         stencil_sizes,  &
+                                         stencil_max,    &
+                                         stencil_map,    &
+                                         depth,          &
+                                         ndf_out,        &
+                                         undf_out,       &
+                                         map_out,        &
+                                         ndf_in,         &
+                                         undf_in,        &
                                          map_in)
 
   use boundaries_config_mod,        only : blending_weights, &
@@ -79,52 +83,55 @@ subroutine set_blending_weights_w2_code( nlayers,       &
   implicit none
 
   ! Arguments
-  integer(kind=i_def),                                 intent(in)    :: nlayers
-  integer(kind=i_def),                                 intent(in)    :: ndf_out, undf_out
-  integer(kind=i_def),                                 intent(in)    :: ndf_in, undf_in
-  real(kind=r_def), dimension(undf_out),               intent(inout) :: weights_field
-  real(kind=r_def), dimension(undf_in),                intent(in)    :: onion_layers
-  integer(kind=i_def),                                 intent(in)    :: stencil_size
-  integer(kind=i_def), dimension(ndf_in,stencil_size), intent(in)    :: stencil_map
-  integer(kind=i_def),                                 intent(in)    :: depth
-  integer(kind=i_def), dimension(ndf_out),             intent(in)    :: map_out
-  integer(kind=i_def), dimension(ndf_in),              intent(in)    :: map_in
+  integer(kind=i_def),  intent(in)    :: nlayers
+  integer(kind=i_def),  intent(in)    :: ndf_out, undf_out
+  integer(kind=i_def),  intent(in)    :: ndf_in, undf_in
+  integer(kind=i_def),  intent(in)    :: stencil_max
+  real(kind=r_def),     intent(inout) :: weights_field(undf_out)
+  real(kind=r_def),     intent(in)    :: onion_layers(undf_in)
+  integer(kind=i_def),  intent(in)    :: stencil_sizes(4)
+  integer(kind=i_def),  intent(in)    :: stencil_map(ndf_in,stencil_max,4)
+  integer(kind=i_def),  intent(in)    :: depth
+  integer(kind=i_def),  intent(in)    :: map_out(ndf_out)
+  integer(kind=i_def),  intent(in)    :: map_in(ndf_in)
 
   ! Internal variables
   integer(kind=i_def) :: k, df
   integer(kind=i_def) :: index
-  integer(kind=i_def) :: onion_layer, cell_next_layer
+  integer(kind=i_def) :: onion_layer
 
-  onion_layer = int(onion_layers(stencil_map(1,1)))
+  onion_layer = int(onion_layers(map_in(1)), i_def)
 
   ! W2 weights should not go right up to the inner region
   if (onion_layer > 0_i_def)then
     index = depth - onion_layer + 1
 
     ! Vertical dofs first - these are all set to the W2V blending weights
-
-    do k=0,nlayers-1
-      do df=B,T
+    do k = 0, nlayers-1
+      do df = B, T
         weights_field(map_out(df)+k) = blending_weights_w2v(index)
       end do
     end do
 
     ! Next the horizontal dofs - using W2H blending weights
-
-    do k=0,nlayers-1
-      do df=1,stencil_size-1
-        cell_next_layer = int(onion_layers(stencil_map(1,df+1)))
-        ! If the onion_layer > 1, then we write to all dofs
-        ! If the onion_layer == 1, then only write to dofs with
-        ! neighbouring cells that are within the blending region
-        if ( cell_next_layer > 0_i_def .or. onion_layer > 1_i_def ) then
-          ! W2 weights are averages of neighouring W3 weights
-          weights_field(map_out(df)+k) = min( 1.0_r_def, weights_field(map_out(df)+k) &
-                                                   + 0.5_r_def*blending_weights(index) )
+    do k = 0, nlayers-1
+      ! Loop over four sides of cross stencil (for quadratileral cell)
+      do df = 1, 4
+        ! At very edge of domain, set weight to be 1
+        if (stencil_sizes(df) == 1_i_def) then
+          weights_field(map_out(df)+k) = 1.0_r_def
+        else
+          ! Otherwise, take the average weight from neighbouring columns.
+          ! NB: in the domain interior, the blending weight is 0.
+          ! Therefore we can compute the W2 blending weights by adding
+          ! half the weight for each column
+          weights_field(map_out(df)+k) = min( 1.0_r_def,                       &
+              weights_field(map_out(df)+k)                                     &
+              + 0.5_r_def*blending_weights(index)                              &
+          )
         end if
       end do
     end do
-
   end if
 
 end subroutine set_blending_weights_w2_code
