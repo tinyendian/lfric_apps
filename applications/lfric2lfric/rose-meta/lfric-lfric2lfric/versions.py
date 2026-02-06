76f3117112d961bedff2a026dfb2bab28649aafc
Juan Manuel Castillo Sanchez <48921434+ukmo-juan-castillo@users.noreply.github.com>
2026-02-06 16:56:46 +0000

    Generation of lfric2lfric lbcs (#55)
    Co-authored-by: Lottie Turner <120099755+mo-lottieturner@users.noreply.github.com>

--- applications/lfric2lfric/rose-meta/lfric-lfric2lfric/versions.py
+++ applications/lfric2lfric/rose-meta/lfric-lfric2lfric/versions.py
@@ -21,13 +21,10 @@ class UpgradeError(Exception):
 
 """
 Copy this template and complete to add your macro
-
 class vnXX_txxx(MacroUpgrade):
     # Upgrade macro for <TICKET> by <Author>
-
     BEFORE_TAG = "vnX.X"
     AFTER_TAG = "vnX.X_txxx"
-
     def upgrade(self, config, meta_config=None):
         # Add settings
         return config, self.reports
@@ -44,7 +41,6 @@ class vn30_t99(MacroUpgrade):
         # Commands From: rose-meta/lfric-lfric_atm
         """Set segmentation size for Gregory-Rowntree convection kernel"""
         self.add_setting(config, ["namelist:physics", "conv_gr_segment"], "16")
-
         return config, self.reports
 
 
@@ -124,7 +120,6 @@ class vn30_t146(MacroUpgrade):
         self.add_setting(
             config, ["namelist:jules_surface", "l_point_data"], ".false."
         )
-
         return config, self.reports
 
 
@@ -137,7 +132,6 @@ class vn30_t135(MacroUpgrade):
     def upgrade(self, config, meta_config=None):
         # Commands From: rose-meta/socrates-radiation
         self.add_setting(config, ["namelist:cosp", "n_cosp_step"], "1")
-
         return config, self.reports
 
 
@@ -152,6 +146,28 @@ class vn30_t171(MacroUpgrade):
         # Add adjust_tracer_equation to transport namelist
         self.add_setting(
             config, ["namelist:transport", "adjust_tracer_equation"], ".false."
+        )
+        return config, self.reports
+
+
+class vn30_t48(MacroUpgrade):
+    """Upgrade macro for ticket #48 by Juan M Castillo."""
+
+    BEFORE_TAG = "vn3.0_t171"
+    AFTER_TAG = "vn3.0_t48"
+
+    def upgrade(self, config, meta_config=None):
+        # Commands From: rose-meta/lfric-lfric2lfric
+        self.add_setting(config, ["namelist:lfric2lfric", "mode"], "'ics'")
+        self.add_setting(
+            config,
+            ["namelist:lfric2lfric", "source_file_lbc"],
+            "'source_file_lbc'",
+        )
+        self.add_setting(
+            config,
+            ["namelist:lfric2lfric", "weight_file_lbc"],
+            "'weight_file_lbc'",
         )
 
         return config, self.reports
